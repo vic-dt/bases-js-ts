139100b5828fea78fa9c79fdbf34d31a590a6b55
vic-dt <victoriavr1323@gmail.com>
2025-02-06 19:36:14 +0100

    Constantes, variables y objetos

---
+++ src/bases/01-const-let.ts
@@ -0,0 +1,13 @@
+
+let urbs: string = 'Valencia';
+urbs = 'Madrid';
+
+const aetas: number = 24;
+
+const nomen = 'Tony';
+
+const cognomen = 'Stark';
+
+export const plenumNomen: string = `${nomen} ${cognomen} tiene ${aetas*2} años y vive en ${urbs}`;
+
+console.log(plenumNomen)
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import './bases/01-const-let'
+import './bases/02-object'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -11,14 +12,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
+    
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
