6bde6ec0b1907c128adfc69524af94bf33e365dd
vic-dt <victoriavr1323@gmail.com>
2025-02-06 20:42:23 +0100

    desestructuracion de objetos

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import viteLogo from '/vite.svg'
 //import './bases/01-const-let'
 //import './bases/02-object'
 //import './bases/03-arrays'
-import './bases/04-functions'
+//import './bases/04-functions'
+import './bases/05-deses-obj'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
